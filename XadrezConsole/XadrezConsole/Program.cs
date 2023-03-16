c739947db5255b51f84640225a7e191a5121d53a
luccalkm <102185385+luccalkm@users.noreply.github.com>
2023-03-16 17:00:56 -0300

    Better Console Chest

---
+++ XadrezConsole/XadrezConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using XadrezConsole.BoardNS;
+using XadrezConsole;
+using XadrezConsole.Xadrez;
+
+namespace XadrezConsole;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Board board = new Board(8, 8);
+        Screen.PrintBoard(board);
+    }
+}
